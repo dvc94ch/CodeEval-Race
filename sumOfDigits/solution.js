80e949d99db00b9c6232c0c89adf0180e348c46f
David Craven <david@craven.ch>
2015-07-18 19:09:33 -0700

    Sum of Numbers

---
+++ sumOfDigits/solution.js
@@ -0,0 +1,14 @@
+var fs  = require("fs");
+var filename = process.argv[2] || 'test.txt';
+
+fs.readFileSync(filename).toString().split('\n').forEach(function (line) {
+  if (line != "") {
+    console.log(solution(line));
+  }
+});
+
+function solution(line){
+  return line.split('').map(Number).reduce(function(acc, num) {
+    return acc + num;
+  });
+}
